d7a73f0f71fcc763faaa09add694da88ce36050f
Atul Jain <83856750+atuljain1383@users.noreply.github.com>
2021-05-22 21:17:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors());
-const port = process.env.port;
 
 app.use(express.json()); //express.json() is a built express middleware that convert request body to JSON.
 app.use("/program", programRoute);
 app.use("/student", studentRoute);
 app.use("/login", loginRoute);
-
+const port = process.env.port || 9000;
 app.listen(port, () =>{
     console.log(`listening to port ${port}`);
 });
